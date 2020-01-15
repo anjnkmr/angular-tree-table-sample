2cfbf742bf0d9d017edacbdc8707539144c4d0f6
Anjan Kumar <kumar.rakuditi@gmail.com>
2020-01-15 07:31:35 +0530

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BasicComponent } from './components/basic/basic.component';
+import { ExpandableComponent } from './components/expandable/expandable.component';
+import { ServerSideComponent } from './components/server-side/server-side.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'basic',
+    pathMatch: 'full'
+  },
+  {
+    path: 'basic',
+    component: BasicComponent
+  },
+  {
+    path: 'expandable',
+    component: ExpandableComponent
+  },
+  {
+    path: 'server-side',
+    component: ServerSideComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AngularTreeTableModule } from 'angular-tree-table';
 import { AppComponent } from './app.component';
+import { BasicComponent } from './components/basic/basic.component';
+import { ExpandableComponent } from './components/expandable/expandable.component';
+import { ServerSideComponent } from './components/server-side/server-side.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicComponent,
+    ExpandableComponent,
+    ServerSideComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularTreeTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
